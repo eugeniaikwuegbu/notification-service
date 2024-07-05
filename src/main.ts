924541703ef800eb15d3a9345affa1b6089f5d1c
Eugenia Ikwuegbu <eugenia@zigah.co>
2024-07-05 21:05:59 +0100

    Update Main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { RmqService } from './modules/rabbitmq/rabbitmq.service';
 
 async function bootstrap() {
   const rmqService = new RmqService();
+  const configService = new ConfigService();
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    rmqService.getOptions(),
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice(rmqService.getOptions());
+
+  await app.startAllMicroservices();
+
+  await app.listen(configService.get('PORT'), () =>
+    console.log(
+      `Notification Service is running on: ${configService.get('PORT')}`,
+    ),
   );
-
-  await app.listen();
 }
 bootstrap();
